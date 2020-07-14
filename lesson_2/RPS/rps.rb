969f99049fadfe87f3c2a158cd9231e071c304eb
Clay Karwisch <clay.karwisch@gmail.com>
2020-07-14 12:24:27 -0400

    expand game to include lizard and spock features

--- lesson_2/RPS/rps.rb
+++ lesson_2/RPS/rps.rb
@@ -1,13 +1,16 @@
 class Player
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :score, :move_log
 
   def initialize
     set_name
+    @score = 0
+    @move_log = []
   end
 end
 
 class Human < Player
   def set_name
+    system("clear") || system("cls")
     n = ""
     loop do
       puts "What's your name?"
@@ -16,17 +19,30 @@ class Human < Player
       puts "Sorry. Must enter a name."
     end
     self.name = n
+    puts
   end
 
   def choose
     choice = nil
     loop do
-      puts "Please choose rock, paper, or scissors:"
+      puts "Please choose rock, paper, scissors, lizard or spock:"
       choice = gets.chomp
-      break if Move::VALUES.include?(choice)
+      break if Move::VALUES.include?(choice.downcase)
       puts "Sorry, that is an invalid choice."
     end
-    self.move = Move.new(choice)
+    make_move(choice.downcase)
+    puts
+  end
+
+  def make_move(choice)
+    case choice
+    when 'rock' then self.move = Rock.new
+    when 'paper' then self.move = Paper.new
+    when 'scissors' then self.move = Scissors.new
+    when 'lizard' then self.move = Lizard.new
+    when 'spock' then self.move = Spock.new
+    end
+    move_log << move
   end
 end
 
@@ -40,43 +56,129 @@ class Computer < Player
   end
 end
 
-class Move
-  VALUES = ['rock', 'paper', 'scissors']
-  attr_reader :value
-
-  def initialize(value)
-    # guard clause if value isn't rps
-    @value = value
+class R2D2 < Computer
+  def set_name
+    @name = 'R2D2'
   end
 
-  def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
-  end
-
-  def <(other_move)
-    (rock? && other_move.paper?) ||
-      (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?)
-  end
-
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-
-  def scissors?
-    @value == 'scissors'
+  def choose
+    self.move = Move.new('rock')
   end
 end
 
-class Rule
+class Hal < Computer
+  HAL_CHOICES = ['scissors', 'scissors', 'scissors', 'scissors', 'rock']
+  def set_name
+    @name = 'Hal'
+  end
+
+  def choose
+    self.move = Move.new(HAL_CHOICES.sample)
+  end
+end
+
+class C3PO < Computer
+  def set_name
+    @name = 'C3PO'
+  end
+end
+
+class Move
+  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def rock?
+    value == 'rock'
+  end
+
+  def paper?
+    value == 'paper'
+  end
+
+  def scissors?
+    value == 'scissors'
+  end
+
+  def lizard?
+    value == 'lizard'
+  end
+
+  def spock?
+    value == 'spock'
+  end
+end
+
+class Rock < Move
   def initialize
-    # not sure what the "state" of a rule object should be
+    @value = 'rock'
+  end
+
+  def >(other_move)
+    other_move.scissors? || other_move.lizard?
+  end
+
+  def <(other_move)
+    other_move.paper? || other_move.spock?
+  end
+end
+
+class Paper < Move
+  def initialize
+    @value = 'paper'
+  end
+
+  def >(other_move)
+    other_move.rock? || other_move.spock?
+  end
+
+  def <(other_move)
+    other_move.scissors? || other_move.lizard?
+  end
+end
+
+class Scissors < Move
+  def initialize
+    @value = 'scissors'
+  end
+
+  def >(other_move)
+    other_move.paper? || other_move.lizard?
+  end
+
+  def <(other_move)
+    other_move.rock? || other_move.spock?
+  end
+end
+
+class Lizard < Move
+  def initialize
+    @value = 'lizard'
+  end
+
+  def >(other_move)
+    other_move.paper? || other_move.spock?
+  end
+
+  def <(other_move)
+    other_move.scissors? || other_move.rock?
+  end
+end
+
+class Spock < Move
+  def initialize
+    @value = 'spock'
+  end
+
+  def >(other_move)
+    other_move.rock? || other_move.scissors?
+  end
+
+  def <(other_move)
+    other_move.paper? || other_move.lizard?
   end
 end
 
@@ -85,15 +187,26 @@ class RPSGame
 
   def initialize
     @human = Human.new
-    @computer = Computer.new
+    select_random_computer_opponent
+  end
+
+  def select_random_computer_opponent
+    choice = rand(3)
+    case choice
+    when 0 then self.computer = R2D2.new
+    when 1 then self.computer = Hal.new
+    when 2 then self.computer = C3PO.new
+    end
   end
 
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
+    puts "Welcome to Rock, Paper, Scissors, Lizard, Spock!"
+    puts
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors! Goodbye."
+    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock! Goodbye."
+    puts
   end
 
   def display_winner
@@ -104,6 +217,7 @@ class RPSGame
     else
       puts "It's a tie!"
     end
+    puts
   end
 
   def play_again?
@@ -114,12 +228,29 @@ class RPSGame
       break if ['y', 'n'].include?(answer)
       puts "Sorry, must be y or n."
     end
+    puts
     answer == 'y'
   end
 
   def display_moves
     puts "#{human.name} chose #{human.move.value}."
     puts "#{computer.name} chose #{computer.move.value}."
+    puts
+  end
+
+  def update_score
+    if human.move > computer.move
+      human.score += 1
+    elsif human.move < computer.move
+      computer.score += 1
+    end
+  end
+
+  def display_score
+    puts "The current score is:"
+    puts "#{human.name}: #{human.score}"
+    puts "#{computer.name}: #{computer.score}"
+    puts
   end
 
   def play
@@ -129,7 +260,9 @@ class RPSGame
       computer.choose
       display_moves
       display_winner
-      break unless play_again?
+      update_score
+      display_score
+      break if human.score == 5 || computer.score == 5 || !play_again?
     end
     display_goodbye_message
   end
